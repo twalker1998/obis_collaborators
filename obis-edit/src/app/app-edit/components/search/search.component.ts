2113ee99c3271ed5787db1588914185bb93b15e5
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-01-20 23:08:25 +0200

    Added logic back to search component

--- obis-edit/src/app/app-edit/components/search/search.component.ts
+++ obis-edit/src/app/app-edit/components/search/search.component.ts
@@ -5,9 +5,9 @@ import { Acctax } from '../../../shared/models/acctax';
 import { Comtax } from '../../../shared/models/comtax';
 import { Syntax } from '../../../shared/models/syntax';
 
-// import { SearchService } from '../../core/search/search.service';
-// import { ResultsService } from '../../core/results/results.service';
-// import { MapService } from '../../core/map/map.service';
+import { SearchService } from '../../core/search/search.service';
+import { ResultsService } from '../../core/results/results.service';
+import { MapService } from '../../core/map/map.service';
 
 @Component({
   selector: 'app-search',
@@ -23,92 +23,92 @@ export class SearchComponent implements AfterViewInit {
   page = 1;
   pageSize = 15;
 
-  // @HostListener('window:popstate', ['$event'])
-  // onPopState(event: any) {
-  //   let results_str = localStorage.getItem("results");
-  //   this.results = JSON.parse(results_str);
-  //   this.resultsService.isQueryStarted.next(false);
-  //   this.resultsService.isQueryComplete.next(true);
-  //   this.mapService.changeAcode("search");
-  // }
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event: any) {
+    let results_str = localStorage.getItem("results");
+    this.results = JSON.parse(results_str);
+    this.resultsService.isQueryStarted.next(false);
+    this.resultsService.isQueryComplete.next(true);
+    this.mapService.changeAcode("search");
+  }
 
-  constructor(/*private searchService: SearchService, private resultsService: ResultsService, private mapService: MapService,*/ private router: Router) {
-    // this.resultsService.isQueryStarted.subscribe(s_value => {
-    //   this.isQueryStarted = s_value;
-    // });
+  constructor(private searchService: SearchService, private resultsService: ResultsService, private mapService: MapService, private router: Router) {
+    this.resultsService.isQueryStarted.subscribe(s_value => {
+      this.isQueryStarted = s_value;
+    });
 
-    // this.resultsService.isQueryComplete.subscribe(c_value => {
-    //   if(c_value) {
-    //     this.isQueryStarted = false;
-    //     if(this.searchService.get_results().length == 0) {
-    //       let results_str = localStorage.getItem("results");
+    this.resultsService.isQueryComplete.subscribe(c_value => {
+      if(c_value) {
+        this.isQueryStarted = false;
+        if(this.searchService.get_results().length == 0) {
+          let results_str = localStorage.getItem("results");
 
-    //       if(!results_str) {
-    //         this.results = new Array<Acctax | Comtax | Syntax>();
-    //       } else {
-    //         this.results = JSON.parse(results_str);
-    //       }
+          if(!results_str) {
+            this.results = new Array<Acctax | Comtax | Syntax>();
+          } else {
+            this.results = JSON.parse(results_str);
+          }
 
-    //       this.searchService.set_results(this.results);
-    //       this.searchService.get_taxa_strings();
-    //     } else {
-    //       this.results = this.searchService.get_results();
-    //       let results_str = JSON.stringify(this.results);
-    //       localStorage.setItem("results", results_str);
-    //     }
-    //   }
+          this.searchService.set_results(this.results);
+          this.searchService.get_taxa_strings();
+        } else {
+          this.results = this.searchService.get_results();
+          let results_str = JSON.stringify(this.results);
+          localStorage.setItem("results", results_str);
+        }
+      }
 
-    //   this.isQueryComplete = c_value;
-    // });
+      this.isQueryComplete = c_value;
+    });
   }
 
   ngAfterViewInit() {
-    // var searchBar: HTMLElement;
-    // var submitSearch: HTMLElement;
+    var searchBar: HTMLElement;
+    var submitSearch: HTMLElement;
 
-    // if(this.location === 'main') {
-    //   searchBar = document.getElementById("query_main");
-    //   submitSearch = document.getElementById("submitSearch_main");
-    // } else if(this.location === 'map') {
-    //   searchBar = document.getElementById("query_map");
-    //   submitSearch = document.getElementById("submitSearch_map");
-    // }
+    if(this.location === 'main') {
+      searchBar = document.getElementById("query_main");
+      submitSearch = document.getElementById("submitSearch_main");
+    } else if(this.location === 'map') {
+      searchBar = document.getElementById("query_map");
+      submitSearch = document.getElementById("submitSearch_map");
+    }
 
-    // searchBar.addEventListener("keyup", function(event) {
-    //   if(event.keyCode === 13) {
-    //     event.preventDefault();
+    searchBar.addEventListener("keyup", function(event) {
+      if(event.keyCode === 13) {
+        event.preventDefault();
 
-    //     submitSearch.click();
-    //   }
-    // });
+        submitSearch.click();
+      }
+    });
     
-    // this.searchService.query.subscribe(query => {
-    //   this.updateQueryValue(query);
-    // });
+    this.searchService.query.subscribe(query => {
+      this.updateQueryValue(query);
+    });
   }
 
   search(query: string): void {
-    // this.clearResult();
-    // this.page = 1;
-    // this.searchService.query_api(query);
-    // this.resultsService.isQueryStarted.next(true);
-    // this.resultsService.isQueryComplete.next(false);
-    // this.mapService.changeAcode("search");
-    // this.searchService.updateQuery(query);
+    this.clearResult();
+    this.page = 1;
+    this.searchService.query_api(query);
+    this.resultsService.isQueryStarted.next(true);
+    this.resultsService.isQueryComplete.next(false);
+    this.mapService.changeAcode("search");
+    this.searchService.updateQuery(query);
   }
 
   clearResult(): void {
-    // this.results = new Array<Acctax | Comtax | Syntax>();
-    // localStorage.removeItem("results");
-    // this.router.navigate(["./"]);
-    // this.searchService.updateQuery("");
+    this.results = new Array<Acctax | Comtax | Syntax>();
+    localStorage.removeItem("results");
+    this.router.navigate(["./"]);
+    this.searchService.updateQuery("");
   }
 
   updateQueryValue(query: string) {
-  //   if(this.location === 'main') {
-  //     (<HTMLInputElement>document.getElementById("query_map")).value = query;
-  //   } else if(this.location === 'map') {
-  //     (<HTMLInputElement>document.getElementById("query_main")).value = query;
-  //   }
+    if(this.location === 'main') {
+      (<HTMLInputElement>document.getElementById("query_map")).value = query;
+    } else if(this.location === 'map') {
+      (<HTMLInputElement>document.getElementById("query_main")).value = query;
+    }
   }
 }
