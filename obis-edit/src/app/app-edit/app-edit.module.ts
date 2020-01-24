fffa569a78e1be4d5fff05e29fc1c9121a8eb5cb
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-01-24 15:42:44 -0600

    Added declaration for distribution map component

--- obis-edit/src/app/app-edit/app-edit.module.ts
+++ obis-edit/src/app/app-edit/app-edit.module.ts
@@ -7,8 +7,10 @@ import { EditMainComponent } from './components/edit-main/edit-main.component';
 import { SearchComponent } from './components/search/search.component';
 import { ResultComponent } from './components/result/result.component';
 
+import { DistributionMapComponent } from './components/distribution-map/distribution-map.component';
+
 @NgModule({
-  declarations: [EditMainComponent, SearchComponent, ResultComponent],
+  declarations: [EditMainComponent, SearchComponent, ResultComponent, DistributionMapComponent],
   imports: [
     CommonModule,
     SharedModule,
