a71cca7451de6b4a6cbb3bc35479104b63139383
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-01-30 21:18:24 -0600

    Added RecordService and function to check acode uniqueness

---
+++ obis-edit/src/app/app-edit/core/record/record.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecordService {
+  constructor(private httpClient: HttpClient) { }
+
+  isAcodeUnique(acode: string) {
+    return this.httpClient.get('http://obsvweb1.ou.edu/obis_search_old/check-acode.php?acode=' + acode);
+  }
+
+  private handleError(error: any) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred: ', error.error.message);
+      return throwError('Something bad happened, please try again later.');
+    } else {
+      console.error('Backend returned code ' + error.status + ', body was: ' + error.error);
+      return throwError('Something bad happened, please try again later.');
+    }
+  }
+}
