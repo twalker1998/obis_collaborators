8560199d0edffa161e281286f3b828e82dbdc781
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-02-05 19:46:07 -0600

    Changed AcodeValidator to implement Validator interface

--- obis-edit/src/app/app-edit/validators/acode-validator.ts
+++ obis-edit/src/app/app-edit/validators/acode-validator.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validator } from '@angular/forms';
 
 import { RecordService } from '../core/record/record.service';
 
 @Injectable()
-export class AcodeValidator {
+export class AcodeValidator implements Validator {
   debouncer: any;
 
   constructor(private recordService: RecordService) { }
 
-  isAcodeUnique(control: FormControl): any {
+  validate(control: FormControl): any {
     clearTimeout(this.debouncer);
 
     return new Promise(resolve => {
