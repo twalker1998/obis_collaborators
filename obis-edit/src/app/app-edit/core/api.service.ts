75fa76284773b2c0a80b39f283e8dfbade8b4304
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-02-08 22:22:21 -0600

    Removed unnecessary import

--- obis-edit/src/app/app-edit/core/api.service.ts
+++ obis-edit/src/app/app-edit/core/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { throwError, Observable } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
